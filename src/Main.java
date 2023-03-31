02af4d59eeba02a9276930619f21ca60695cff59
Роман <110688570+MoskalchenkoRoman@users.noreply.github.com>
2023-03-31 13:12:55 +0300

    dz3

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,15 @@
+import java.util.LinkedHashMap;
+
 public class Main {
     public static void main(String[] args) {
 
         System.out.println("Hello world!");
+    }
+    LinkedHashMap<> gitl = new LinkedHashMap(10);
+    public LinkedHashMap getGitl() {
+        return gitl;
+    }
+    public void setGitl(LinkedHashMap gitl) {
+        this.gitl = gitl;
     }
 }
